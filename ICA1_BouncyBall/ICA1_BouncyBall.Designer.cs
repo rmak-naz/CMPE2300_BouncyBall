75c6acc297eb07ac45b15ba9db209739b300d676
rmak-naz <40699936+rmak-naz@users.noreply.github.com>
2018-11-27 21:31:14 -0700

    Add files via upload

---
+++ ICA1_BouncyBall/ICA1_BouncyBall.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ICA1_BouncyBall
+{
+    partial class ICA1_BouncyBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.B_RandBall = new System.Windows.Forms.Button();
+            this.B_HyperBall = new System.Windows.Forms.Button();
+            this.Bounce_Intrvl = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // B_RandBall
+            // 
+            this.B_RandBall.Location = new System.Drawing.Point(13, 13);
+            this.B_RandBall.Name = "B_RandBall";
+            this.B_RandBall.Size = new System.Drawing.Size(259, 23);
+            this.B_RandBall.TabIndex = 0;
+            this.B_RandBall.Text = "Add Random Ball";
+            this.B_RandBall.UseVisualStyleBackColor = true;
+            this.B_RandBall.Click += new System.EventHandler(this.B_RandBall_Click);
+            // 
+            // B_HyperBall
+            // 
+            this.B_HyperBall.Location = new System.Drawing.Point(13, 43);
+            this.B_HyperBall.Name = "B_HyperBall";
+            this.B_HyperBall.Size = new System.Drawing.Size(259, 23);
+            this.B_HyperBall.TabIndex = 1;
+            this.B_HyperBall.Text = "Add HyperBall";
+            this.B_HyperBall.UseVisualStyleBackColor = true;
+            this.B_HyperBall.Click += new System.EventHandler(this.B_HyperBall_Click);
+            // 
+            // Bounce_Intrvl
+            // 
+            this.Bounce_Intrvl.Enabled = true;
+            this.Bounce_Intrvl.Interval = 20;
+            this.Bounce_Intrvl.Tick += new System.EventHandler(this.Bounce_Intrvl_Tick);
+            // 
+            // ICA1_BouncyBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 70);
+            this.Controls.Add(this.B_HyperBall);
+            this.Controls.Add(this.B_RandBall);
+            this.Name = "ICA1_BouncyBall";
+            this.Text = "BouncyBall";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_RandBall;
+        private System.Windows.Forms.Button B_HyperBall;
+        private System.Windows.Forms.Timer Bounce_Intrvl;
+    }
+}
+
---
+++ ICA1_BouncyBall/ICA1_BouncyBall.cs
@@ -0,0 +1,79 @@
+﻿//*********************************************************************
+//Program: ICA 1 - Bouncy Balls
+//Files: Program.cs
+// ICA1_BouncyBall.cs
+// ICA1_BouncyBall.Designer.cs
+// ICA1_BouncyBall.resx
+// ICA1_Balls.cs
+//Description: Generates random balls to bounce on the screen
+//Version: 1.0
+//Date: Sept 14, 2016
+//Author: Ronald Mak
+//Class: CMPE 2300
+//Instructor: Simon Walker
+//*********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+
+namespace ICA1_BouncyBall
+{
+    public partial class ICA1_BouncyBall : Form
+    {
+        private CDrawer _canvas = new CDrawer();                //Initialize global canvas to draw
+        private List<CBall> _ballList = new List<CBall>();      //Iterate through list to draw each ball on canvas
+        private Random rndNum = new Random();
+        
+        public ICA1_BouncyBall()
+        {
+            InitializeComponent();
+            _canvas.ContinuousUpdate = false;
+        }
+
+        //Generate a random point where X != 0, Y != 0, X != Y
+        private Point GetRandPos()
+        {
+            Point rndPos = new Point();
+
+            do
+            {
+                rndPos.X = rndNum.Next(-5, 5);
+                rndPos.Y = rndNum.Next(-5, 5);
+            } while (rndPos.X == 0 || rndPos.Y == 0 || rndPos.X == rndPos.Y);
+
+            return rndPos;
+        }
+
+        // Add a new ball into the list through leveraged constructor with color and direction
+        private void B_RandBall_Click(object sender, EventArgs e)
+        {
+            _ballList.Add(new CBall(RandColor.GetColor(), GetRandPos()));
+        }
+
+        // Add a new ball into the list with regular constructor
+        private void B_HyperBall_Click(object sender, EventArgs e)
+        {
+            _ballList.Add(new CBall(RandColor.GetColor()));
+        }
+
+        private void Bounce_Intrvl_Tick(object sender, EventArgs e)
+        {
+            _canvas.Clear();
+            foreach (CBall ball in _ballList)
+            {
+                ball.Render(_canvas);
+                _canvas.Render();
+                ball.Move();
+            }
+        }
+    }
+}
